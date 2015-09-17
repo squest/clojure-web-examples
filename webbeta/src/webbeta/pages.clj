a29ad82eb295c824091abd26c28c507603fd95f5
squest <sabdaps@gmail.com>
2015-09-17 20:35:54 +0700

    @skadinyo @aisyahgunung pages

--- webbeta/src/webbeta/pages.clj
+++ webbeta/src/webbeta/pages.clj
@@ -3,22 +3,68 @@
     [hiccup.core :as hc]
     [hiccup.page :as hp]))
 
+(defn- head
+  []
+  (hc/html [:head
+            [:meta {:charset "utf-8"}]
+            (hp/include-css "/css/normalize.css")
+            (hp/include-css "/css/foundation.min.css")]))
+
+(defn- header
+  []
+  (hc/html [:header {:class "large-12 large-centered columns"}
+            [:h2 "Some logo"]
+            [:hr]]))
+
+(defn- footer
+  []
+  (hc/html [:footer
+            [:hr]
+            [:p "Copyright 2015 Jojon dkk"]]))
+
+(defn- flat-thing
+  [title some-fake-image]
+  (hc/html
+    [:div.large-4.column
+     [:h4 title]
+     [:p some-fake-image]]))
+
+(defn- body
+  []
+  (hc/html [:div {:class "large-12"}
+            [:h1 "Some article"]
+            [:div {:class "large-12 columns"}
+             (map flat-thing
+                  ["First title" "Second title" "Third title"]
+                  ["fake one" "fake two" "fake three"])
+             (for [i (map vector
+                          ["First title" "Second title" "Third title"]
+                          ["fake one" "fake two" "fake three"])]
+               (apply flat-thing i))]]))
+
 (defn home
-  ([] (hp/html5
-        [:head
-         [:meta {:charset "utf-8"}]
-         (hp/include-css "/css/normalize.css")
-         (hp/include-css "/css/foundation.min.css")]
-        [:body
-         [:center
-          [:h1 "Hellooow world!"]]]))
-  ([nama]
-   (hp/html5
-     [:head
-      [:meta {:charset "utf-8"}]
-      (hp/include-css "/css/normalize.css")
-      (hp/include-css "/css/foundation.min.css")]
-     [:body
-      [:center
-       [:h1 (str "Hello " nama)]]])))
+  ([] (hp/html5 (head)
+                [:body {:class "row"}
+                 (header)
+                 (body)
+                 (footer)]))
+  ([nama] (hp/html5 (head)
+                [:body {:class "row"}
+                 (header)
+                 (body)
+                 [:h2 (str "Hello Wala")]
+                 (footer)])))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
